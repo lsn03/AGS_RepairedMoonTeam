eb357cee31d6ee9fa953157c0704dee7a04d7e41
lsn03 <85879960+lsn03@users.noreply.github.com>
2022-04-08 11:25:38 +1000

    Добавил PhotonPun и переход из лобби в игру

---
+++ Assets/Scripts/Network/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject player;
+    private void Start()
+    {
+        PhotonNetwork.Instantiate( player.name, new Vector2(Random.Range(-6,6),-2 ),Quaternion.identity);
+    }
+    public void Leave()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    public override void OnLeftLobby()
+    {
+        SceneManager.LoadScene( "Lobby" );
+    }
+}
---
+++ Assets/Scripts/Network/LobbyManager.cs
@@ -0,0 +1,50 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Text TextName;
+    [SerializeField] private Text TextLog;
+
+    private void Start()
+    {
+        TextName.text = PlayerPrefs.GetString( "name" );
+        PhotonNetwork.NickName = TextName.text;
+        Log( "welcome" );
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Log( "Connect" );
+    }
+
+    public void CreateRoom()
+    {
+        SaveName();
+        Log( "Create" );
+        PhotonNetwork.CreateRoom( null, new Photon.Realtime.RoomOptions { MaxPlayers = 2 } );
+    }
+
+    public void JoinRoom()
+    {
+        SaveName();
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnJoinedRoom()
+    {
+        Log( "Join" );
+        PhotonNetwork.LoadLevel( "Game" );
+    }
+    private void SaveName()
+    {
+        PlayerPrefs.SetString( "name", TextName.text );
+    }
+    private void Log(string message)
+    {
+        TextLog.text = message;
+    }
+}
