94323ed0472f76d65a72e8a68101c6d6ac3650a5
lsn03 <85879960+lsn03@users.noreply.github.com>
2022-04-12 22:22:14 +1000

    стрельба рейкастом сделана полностью

--- Assets/Scripts/Player/Gun/SingleShot.cs
+++ Assets/Scripts/Player/Gun/SingleShot.cs
@@ -8,11 +8,13 @@ public class SingleShot : Gun
     [SerializeField] Camera cam;
     public Transform bulletSpawn;
 
+    public LineRenderer lineRenderer;
+
     public override void Use()
     {
-        Shoot();
+        StartCoroutine( Shoot());
     }
-    void Shoot()
+    IEnumerator Shoot()
     {
        
         RaycastHit2D hitInfo = Physics2D.Raycast( bulletSpawn.position, bulletSpawn.right );
@@ -21,7 +23,16 @@ public class SingleShot : Gun
         {
             Debug.Log( hitInfo.transform.name );
             hitInfo.collider.gameObject.GetComponent<IDamage>()?.TakeDamage(((GunIno)itemInfo).damage);
+            lineRenderer.SetPosition( 0, bulletSpawn.position );
+            lineRenderer.SetPosition( 1, hitInfo.point );
         }
-        
+        else
+        {
+            lineRenderer.SetPosition( 0, bulletSpawn.position );
+            lineRenderer.SetPosition( 1, bulletSpawn.position + bulletSpawn.right * 50 ) ;
+        }
+        lineRenderer.enabled = true;
+        yield return new WaitForSeconds( 0.02f );
+        lineRenderer.enabled = false;
     }
 }
