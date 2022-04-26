10fc860756beeceb5f6276b47387122e2b4fe420
lsn03 <85879960+lsn03@users.noreply.github.com>
2022-04-26 22:00:53 +1000

    начата разработка системы патронов Добавлена отрисовка количества патронов

--- Assets/Scripts/Player/Gun/SingleShot.cs
+++ Assets/Scripts/Player/Gun/SingleShot.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class SingleShot : Gun
 {
@@ -13,13 +14,24 @@ public class SingleShot : Gun
     public PhotonView photonView;
     public float timeBetweenShoots;
     float timeBeforeShoots;
+    public int magazineSize;
+    int bulletsLeft, bulletsShot;
+    float reloadTime;
+    const int maxBullets = 200;
+    bool reloading;
+
+    public TextMeshProUGUI text;
 
     [Range(0,0.5f),SerializeField] float waitForSeconds;
     public override void Use()
     {
         
     }
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
 
+    }
     void Start()
     {
        
@@ -34,7 +46,9 @@ public class SingleShot : Gun
 
         if ( timeBeforeShoots <= 0 )
         {
-            if ( Input.GetMouseButtonDown( 0 ) && itemGameObject.active )
+            if ( Input.GetKeyDown( KeyCode.R ) && bulletsLeft < magazineSize && !reloading ) Reload();
+
+            if ( Input.GetMouseButtonDown( 0 ) && itemGameObject.active &&!reloading && bulletsLeft>0)
             {
                 photonView.RPC( "Shoot", RpcTarget.All );
                 timeBeforeShoots = timeBetweenShoots;
@@ -44,38 +58,51 @@ public class SingleShot : Gun
         {
             timeBeforeShoots -= Time.deltaTime;
         }
+        text.SetText( bulletsLeft + " / " + magazineSize );
+    }
+
+    void Reload()
+    {
+        reloading = true;
+        Invoke( "ReloadFinished", reloadTime );
+    }
+
+    void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
     }
 
     [PunRPC]
     IEnumerator Shoot()
     {
-        
-            //Physics2D.queriesStartInColliders = false;
-            RaycastHit2D hitInfo = Physics2D.Raycast( bulletSpawn.position, bulletSpawn.right );
 
-            if ( hitInfo )
+        //Physics2D.queriesStartInColliders = false;
+        RaycastHit2D hitInfo = Physics2D.Raycast( bulletSpawn.position, bulletSpawn.right );
+        bulletsLeft--;
+        if ( hitInfo )
+        {
+            Instantiate( hitEffect, hitInfo.point, Quaternion.identity );
+            Debug.Log( hitInfo.transform.name );
+            if ( photonView.IsMine )
             {
-                Instantiate( hitEffect, hitInfo.point, Quaternion.identity );
-                Debug.Log( hitInfo.transform.name );
-                if ( photonView.IsMine )
-                {
-                    hitInfo.collider.gameObject.GetComponent<IDamage>()?.TakeDamage( ( ( GunIno )itemInfo ).damage );
-                }
-                lineRenderer.SetPosition( 0, bulletSpawn.position );
-                lineRenderer.SetPosition( 1, hitInfo.point );
+                hitInfo.collider.gameObject.GetComponent<IDamage>()?.TakeDamage( ( ( GunIno )itemInfo ).damage );
             }
-            else
-            {
-                lineRenderer.SetPosition( 0, bulletSpawn.position );
-                lineRenderer.SetPosition( 1, bulletSpawn.position + bulletSpawn.right * 50 );
-            }
+            lineRenderer.SetPosition( 0, bulletSpawn.position );
+            lineRenderer.SetPosition( 1, hitInfo.point );
+        }
+        else
+        {
+            lineRenderer.SetPosition( 0, bulletSpawn.position );
+            lineRenderer.SetPosition( 1, bulletSpawn.position + bulletSpawn.right * 50 );
+        }
 
-            timeBeforeShoots = timeBetweenShoots;
-            if ( lineRenderer != null )
-                lineRenderer.enabled = true;
-            yield return new WaitForSeconds( waitForSeconds );
-            if ( lineRenderer != null )
-                lineRenderer.enabled = false;
-        
+        timeBeforeShoots = timeBetweenShoots;
+        if ( lineRenderer != null )
+            lineRenderer.enabled = true;
+        yield return new WaitForSeconds( waitForSeconds );
+        if ( lineRenderer != null )
+            lineRenderer.enabled = false;
+
     }
 }
