6d57c74a3263b6c4425f05af6a93b4c82e83a595
DeniskaWolk <93892241+DeniskaWolk@users.noreply.github.com>
2022-08-08 19:24:44 +1000

    final (hopefully) jumppad changes
    another one
    so now her is 3 options how x velocity is calculated
    first is using velocity from jump pad
    second is using velocity from player
    third is using biggest

--- Assets/Scripts/Player/JumpPad.cs
+++ Assets/Scripts/Player/JumpPad.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     public Vector2 jumpPadForce;
-    public bool isPreservePlayerSpeed;
-    private void OnCollisionEnter2D( Collision2D collision )
+    //если 0 ( по умолчанию), то горизонтальная скорость персонажа заменяется скоростью джам пада, если 1, то берется скорость персонажа, если 2 то берется наибольшее значение
+    //вариант 0 полезен когда мы хотим что бы игрок летел по заданной траэктории, вариант 1 - для вертикальных джам падов, когда мы не хотим что-бы скорость терялась, 2 - для остального
+    [Range(0, 2), SerializeField] int isPreservePlayerSpeed;
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Player") )
+        if (collision.gameObject.CompareTag("Player"))
         {
             Vector2 _finalForce = jumpPadForce;
-            if (isPreservePlayerSpeed)
-                _finalForce.x = collision.gameObject.GetComponent<Rigidbody2D>().velocity.x;
+            float _playerVelocityX = collision.gameObject.GetComponent<Rigidbody2D>().velocity.x;
+            if (isPreservePlayerSpeed == 1)
+            {
+                _finalForce.x = _playerVelocityX;
+            }
+
+            if (isPreservePlayerSpeed == 2)
+                if (_finalForce.x > 0)
+                    _finalForce.x = Mathf.Max(_finalForce.x, _playerVelocityX);
+                else if (_finalForce.x < 0)
+                    _finalForce.x = Mathf.Min(_finalForce.x, _playerVelocityX);
+                else
+                    _finalForce.x = _playerVelocityX;
+
             collision.gameObject.GetComponent<Rigidbody2D>().velocity = _finalForce;
         }
     }
